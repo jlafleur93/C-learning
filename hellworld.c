5480430a459079da5339a93240b4a505108bf615
joel-wowz <josephlafleur@hotmail.com>
2021-08-19 00:18:40 -0600

    added comment and note to hellworld

--- hellworld.c
+++ hellworld.c
@@ -4,3 +4,4 @@ int main(){
   printf("hell world! \n YEP This is hell");
   return 0;
 }
+/* main is special, every program beings executing at the beinning of main, this means eery program must have a main somewhere. It usually calls other functions to help perform its job.*/
